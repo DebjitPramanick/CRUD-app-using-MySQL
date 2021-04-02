f7ecbfb3bc71d5f1d54e3c5359a6605179394ecd
DebjitPramanick <dpramanick9990@gmail.com>
2021-04-02 23:29:25 +0530

    Initialized

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import "./App.css"
+import TextField from '@material-ui/core/TextField';
+import { Button, Card, CardActions, CardContent, Grid, MenuItem, Select, Typography } from '@material-ui/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    const type = ['None', 'Marketer', 'Programmer', 'Designer', 'Content Writer']
+    const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+    const [pos, setPos] = useState(type[0])
+
+    return (
+        <div>
+            <div className="container">
+                <div className="form">
+                    <form className="input-form" noValidate autoComplete="off">
+                        <TextField id="outlined-basic" className="input" label="Outlined" variant="outlined" />
+                        <TextField id="outlined-basic" className="input" label="Outlined" variant="outlined" />
+                        <Select
+                            labelId="demo-simple-select-label"
+                            className="input"
+                            id="demo-simple-select"
+                            value={pos}
+                            onChange={(e) => setPos(e.target.value)}
+                        >
+                            {type.map(t => (
+                                <MenuItem value={t}>{t}</MenuItem>
+                            ))}
+                        </Select>
+                        <Button variant="contained" color="primary"
+                            style={{ marginTop: '20px' }}>
+                            Submit
+                        </Button>
+                    </form>
+                </div>
+
+                <div className="data">
+                    <Grid container spacing={2}>
+                        {cards.map(c => (
+                            <Card className='cards'>
+                                <CardContent>
+                                    <Typography color="textSecondary" gutterBottom>
+                                        Word of the Day
+                                </Typography>
+                                    <Typography variant="h5" component="h2">
+                                        be
+                                </Typography>
+                                    <Typography color="textSecondary">
+                                        adjective
+                                </Typography>
+                                    <Typography variant="body2" component="p">
+                                        well meaning and kindly.
+                                <br />
+                                        {'"a benevolent smile"'}
+                                    </Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <Button size="small">Learn More</Button>
+                                </CardActions>
+                            </Card>
+                        ))}
+                    </Grid>
+
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
